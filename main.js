e903d8da1a635dc06f866a63f441b3f8d170310b
NamSeungOk <qqkddi09@naver.com>
2022-02-13 16:31:10 +0900

    JS
    - 전체삭제 기능 추가

--- main.js
+++ main.js
@@ -1,37 +1,62 @@
 'use strict';
 
+// 선택자 할당
 const todoBtn = document.querySelector('.todo__btn');
 const todoInput = document.querySelector('.todo__input');
 const todoItem = document.querySelector('.todo__items');
+const allDelete = document.querySelector('.footer__btn');
 
-// 사이트 load시 focus 주기
+// 사이트 load시 input focus
 window.onload = function () {
   todoInput.focus();
 };
 
-// todoBtn 클릭시 event 등록
-todoBtn.addEventListener('click', btnClick);
-// input에서 Enter 입력하면 btnClick() 호출
+// 리스트 추가 - Enter
 todoInput.addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
-    btnClick();
+  if (!(event.key === 'Enter')) {
+    return;
+  } else {
+    add();
   }
 });
 
-function btnClick() {
+// 리스트 추가 - (+) 버튼
+todoBtn.addEventListener('click', add);
+
+// 등록한 아이템 전부 삭제
+allDelete.addEventListener('click', () => {
+  if (!confirm('전부 삭제할까요?')) {
+    return;
+  }
+  deleteAllItem();
+});
+
+function add() {
   // 아이템 생성
   const todoList = document.createElement('li');
   todoList.setAttribute('class', 'item');
-  todoList.innerHTML = todoInput.value;
+  todoItem.appendChild(todoList);
+
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item__name');
+  name.innerHTML = todoInput.value;
+  todoList.appendChild(name);
+
+  const itemDiv = document.createElement('div');
+  itemDiv.setAttribute('class', 'item__div');
+  todoList.appendChild(itemDiv);
+
+  // 스크롤링시 list 따라감
+  todoList.scrollIntoView({ behavior: 'smooth' });
+
   // input에 아무 값도 입력하지 않으면 팝업창 띄우고 종료
   if (todoInput.value === '') {
-    alert('입력하세요');
+    alert('할 일을 입력해주세요');
     todoInput.focus();
     return;
   }
-  todoItem.appendChild(todoList);
 
-  // input 초기화 + focus
+  // input 입력 후 초기화 + focus
   todoInput.focus();
   todoInput.value = null;
 
@@ -39,8 +64,12 @@ function btnClick() {
   const removeBtn = document.createElement('button');
   removeBtn.setAttribute('class', 'item__delete');
   removeBtn.innerHTML = `<i class="fa-regular fa-eraser"></i>`;
-  todoList.appendChild(removeBtn);
+  itemDiv.appendChild(removeBtn);
   removeBtn.addEventListener('click', () => {
     todoList.remove();
   });
+}
+
+function deleteAllItem() {
+  todoItem.innerHTML = '';
 }
