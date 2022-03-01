322a64767e34ac6886cc104ef0c44a3f22e1f0fd
NamSeungOk <qqkddi09@naver.com>
2022-03-01 22:09:33 +0900

    Update + Simplify
    - 텍스트 수정시 공백 입력 못하게함
    
    - 코드 단순화 (불필요한 코드 제거)
    
    - if문에서 빠른 return이 되도록 수정

--- main.js
+++ main.js
@@ -1,102 +1,55 @@
 'use strict';
 
-// 선택자 할당
+// Selector
 const todoBtn = document.querySelector('.todo__btn');
 const form = document.querySelector('.form');
 const todoInput = document.querySelector('.todo__input');
 const todoItem = document.querySelector('.todo__items');
 const allDelete = document.querySelector('.delete__btn');
 const recoverBtn = document.querySelector('.recover__btn');
-const updateBtn = document.querySelector('.update__btn');
+const modifyBtn = document.querySelector('.update__btn');
 const header = document.querySelector('.header');
 let deleteItem = [];
 
-// 입력한 정보 저장
+// Save localStorage
 window.addEventListener('beforeunload', () => {
   saveItem();
 });
 
-// 저장된 정보 유지
+// Save localStorage
+function saveItem() {
+  localStorage.setItem('todoItem', todoItem.innerHTML);
+}
+
+// Import localStorage
 window.addEventListener('load', () => {
   clock();
-  todoInput.focus();
   loadItem();
+  todoInput.focus();
 });
 
-// 아이템 등록
+// Import localStorage
+function loadItem() {
+  const loadItem = localStorage.getItem('todoItem');
+  todoItem.innerHTML = loadItem;
+}
+
+// Add Item
 form.addEventListener('click', (evnet) => {
   event.preventDefault();
   if (event.target.tagName === 'INPUT') {
     return;
   } else {
-    add();
-  }
-});
-
-// 아이템 전부 삭제
-allDelete.addEventListener('click', () => {
-  if (
-    !confirm(
-      '전부 삭제하면 복구할 수 없습니다. 정말 삭제하시겠습니까?'
-    )
-  ) {
-    todoInput.focus();
-    return;
-  } else {
-    deleteAllItem();
-  }
-});
-
-// 아이템 복구
-recoverBtn.addEventListener('click', () => {
-  const recoverItem = localStorage.getItem('deleteItem');
-  if (recoverItem === null) {
-    alert('삭제 내역이 없습니다');
-    todoInput.focus();
-    return;
-  }
-
-  if (confirm('이전 삭제내역을 복구하시겠습니까?')) {
-    const recover = recoverItem.replace(/,/g, '');
-    todoItem.insertAdjacentHTML('beforeend', recover);
-    localStorage.removeItem('deleteItem');
-    deleteItem = [];
-    todoInput.focus();
-  } else {
-    todoInput.focus();
-  }
-});
-
-// 텍스트 수정
-updateBtn.addEventListener('click', () => {
-  alert('수정하고 싶은 텍스트를 더블클릭 하세요');
-});
-
-// 텍스트 수정
-todoItem.addEventListener('dblclick', (event) => {
-  if (event.target.classList.contains('item__name')) {
-    const text = event.target
-      .closest('li')
-      .querySelector('.item__name');
-    const renameText = prompt(
-      '텍스트를 수정합니다',
-      text.textContent
-    );
-    if (renameText === null) {
       todoInput.focus();
-      return;
-    } else {
-      text.textContent = renameText;
-      todoInput.focus();
-    }
+    addItem();
   }
 });
 
-// 아이템 등록
-function add() {
+// Add Item
+function addItem() {
   const name = todoInput.value;
-  if (name === '') {
-    alert('할 일을 입력하세요');
+  if (name.trim() === '') {
+    alert('공백은 입력할 수 없습니다');
     todoInput.focus();
     return;
   }
@@ -120,7 +73,7 @@ function createItem(text) {
   name.setAttribute('class', 'item__name');
   name.innerText = text;
 
-  // 아이템 삭제
+  // Delete Item
   const removeBtn = document.createElement('button');
   removeBtn.setAttribute('class', 'item__delete');
   removeBtn.innerHTML = `<i class="fa-regular fa-eraser"></i>`;
@@ -129,54 +82,107 @@ function createItem(text) {
       todoInput.focus();
       return;
     } else {
-      // 삭제하기전 복사
-      const recoverItem = todoList.cloneNode(true);
-      deleteItem.push(recoverItem.outerHTML);
+      // Copy Item
+      const copyItem = todoList.cloneNode(true);
+      console.log(copyItem);
+      deleteItem.push(copyItem.outerHTML);
       localStorage.setItem('deleteItem', deleteItem);
       todoList.remove();
       todoInput.focus();
     }
   });
 
-  // 아이템 체크
-  const cancleBtn = document.createElement('button');
-  cancleBtn.setAttribute('class', 'item__cancle');
-  cancleBtn.innerHTML = `<i class="far fa-calendar-check"></i>`;
-  cancleBtn.addEventListener('click', () => {
-    cancleBtn.parentNode.previousSibling.classList.toggle('line');
+  // Check Item
+  const checkBtn = document.createElement('button');
+  checkBtn.setAttribute('class', 'item__cancle');
+  checkBtn.innerHTML = `<i class="far fa-calendar-check"></i>`;
+  checkBtn.addEventListener('click', () => {
+    checkBtn.parentNode.previousSibling.classList.toggle('line');
     todoInput.focus();
   });
 
   todoList.appendChild(name);
   todoList.appendChild(itemDiv);
-  itemDiv.appendChild(cancleBtn);
+  itemDiv.appendChild(checkBtn);
   itemDiv.appendChild(removeBtn);
 
   return todoList;
 }
 
+// Delete All Items
+allDelete.addEventListener('click', () => {
+  if (
+    !confirm(
+      '전체 삭제하면 복구할 수 없습니다. 정말 삭제하시겠습니까?'
+    )
+  ) {
+    todoInput.focus();
+    return;
+  } else {
+    deleteAllItem();
+  }
+});
+
+// Recovery Item
+recoverBtn.addEventListener('click', () => {
+  const recoverItem = localStorage.getItem('deleteItem');
+  if (recoverItem === null) {
+    alert('삭제 내역이 없습니다');
+    todoInput.focus();
+    return;
+  }
+
+  if (confirm('이전 삭제내역을 복구하시겠습니까?')) {
+    const recover = recoverItem.replace(/,/g, '');
+    todoItem.insertAdjacentHTML('beforeend', recover);
+    localStorage.removeItem('deleteItem');
+    deleteItem = [];
+    todoInput.focus();
+  } else {
+    todoInput.focus();
+  }
+});
+
+// Delete All Items
 function deleteAllItem() {
   todoItem.innerHTML = '';
   localStorage.clear();
   todoInput.focus();
 }
 
-function saveItem() {
-  localStorage.setItem('todoItem', todoItem.innerHTML);
-}
+// Modify Text
+modifyBtn.addEventListener('click', () => {
+  alert('수정하고 싶은 텍스트를 더블클릭 하세요');
+});
 
-function loadItem() {
-  const loadItem = localStorage.getItem('todoItem');
-  todoItem.innerHTML = loadItem;
-}
+// Modify Text
+todoItem.addEventListener('dblclick', (event) => {
+  if (!event.target.classList.contains('item__name')) {
+    return;
+  } else {
+    const text = event.target;
+    const modifyText = prompt(
+      '텍스트를 수정합니다',
+      text.textContent
+    );
+    if (modifyText.trim() === '') {
+      alert('공백으로의 수정은 불가능합니다');
+      todoInput.focus();
+      return;
+    } else {
+      text.textContent = modifyText;
+      todoInput.focus();
+    }
+  }
+});
 
-// 날짜
+// Date
 function clock() {
   const clockTo = document.createElement('h3');
   const clock = new Date();
   const year = clock.getFullYear();
   const month = clock.getMonth() + 1;
   const date = clock.getDate();
-  clockTo.innerText = `${year}년 ${month}월 ${date}일`;
+  clockTo.textContent = `${year}년 ${month}월 ${date}일`;
   header.appendChild(clockTo);
 }
